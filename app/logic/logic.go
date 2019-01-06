d7641e19f4a91dab3ba0deae4d756b0818ba48e4
Fadrian <fadrian.merdhianto@ruma.co.id>
2019-01-06 09:55:35 +0700

    develop #time 1m #comment init

---
+++ app/logic/logic.go
@@ -0,0 +1,17 @@
+package logic
+
+func add(a, b int) int {
+	return a + b
+}
+
+func subtract(a, b int) int {
+	return a + b
+}
+
+func Add(a, b int) int {
+	return add(a, b)
+}
+
+func Subtract(a, b int) int {
+	return a + b
+}
